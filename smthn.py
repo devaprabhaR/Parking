87624a8c02288009c759f52ad55663a1f1490d42
Devaprabha Rajesh <108450133+okwhataboutnow@users.noreply.github.com>
2022-07-05 19:28:27 +0530

    Add files via upload

---
+++ smthn.py
@@ -0,0 +1,21 @@
+#fnc for displaying facility member menu
+def f_menu():
+    print(''*50,'|','-'*50,'|')
+    a='OPERATIONS FOR'
+    aa=a.centre(50)
+    print(''*50,'|',aa,'|')
+    b='FACILITY MEMBER'
+    bb=b.centre(50)
+    print(''*50,'|',bb,'|')
+    print(''*50,'|','-'*50,'|')
+    c='1.Update Records'
+    cc=c.centre(50)
+    print(''*50,'|',cc,'|')
+    d='2.Delete Account'
+    dd=d.centre(50)
+    print(''*50,'|',dd,'|')
+    e='3.Exit'
+    ee=e.centre(50)
+    print(''*50,'|',ee,'|')
+    print(''*50,'|','-'*50,'|')
+    print()
