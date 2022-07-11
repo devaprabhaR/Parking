fcdd11641f7692419a3d5f92231c582c936c176b
Devaprabha Rajesh <108450133+okwhataboutnow@users.noreply.github.com>
2022-07-11 10:48:27 +0530

    Add files via upload

---
+++ customer menu.py
@@ -0,0 +1,33 @@
+#fnc to display customer menu
+
+def c_menu():
+    print(' '*50,'|','-'*50,'|')
+    a='OPERATIONS'
+    aa=a.center(50)
+    print(' '*50,'|',aa,'|')
+    b='FOR CUSTOMER'
+    bb=b.center(50)
+    print(' '*50,'|',bb,'|')
+    print(' '*50,'|','-'*50,'|')
+    c='1.Book A Slot (check in/check out)'
+    cc=c.center(50)
+    print(' '*50,'|',cc,'|')
+    d='2.Cancel Booking'
+    dd=d.center(50)
+    print(' '*50,'|',dd,'|')
+    e='3.Update Records'
+    ee=e.center(50)
+    print(' '*50,'|',ee,'|')
+    f='4.Delete account'
+    ff=f.center(50)
+    print(' '*50,'|',ff,'|')
+    g='5.Payment'
+    gg=g.center(50)
+    print(' '*50,'|',gg,'|')
+    h='6.Exit'
+    hh=h.center(50)
+    print(' '*50,'|',hh,'|')
+    print(' '*50,'|','-'*50,'|')
+    print()
+
+
