afd543897ad8f116542fd8d378e895c5e6b410ee
Devaprabha Rajesh <108450133+okwhataboutnow@users.noreply.github.com>
2022-07-11 10:53:45 +0530

    Add files via upload

---
+++ facility menu.py
@@ -0,0 +1,23 @@
+#fnc for displaying facility member menu
+
+def f_menu():
+    print(' '*50,'|','-'*50,'|')
+    a='OPERATIONS FOR'
+    aa=a.center(50)
+    print(' '*50,'|',aa,'|')
+    b='FACILITY MEMBER'
+    bb=b.center(50)
+    print(' '*50,'|',bb,'|')
+    print(' '*50,'|','-'*50,'|')
+    c='1.Update Records'
+    cc=c.center(50)
+    print(' '*50,'|',cc,'|')
+    d='2.Delete account'
+    dd=d.center(50)
+    print(' '*50,'|',dd,'|')
+    e='3.Exit'
+    ee=e.center(50)
+    print(' '*50,'|',ee,'|')
+    print(' '*50,'|','-'*50,'|')
+    print()
+
